13fe560890051420a9dc46c47ba7cbca8445d98f
LVKhoaGH <lvksaw@gmail.com>
2026-05-11 16:08:31 +0700

    update

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,19 +1,25 @@
 @echo off
-set /p msg="Enter commit message (default: Update GCP Lab Report): "
+echo [CHECK] Checking Git status...
+git status
+
+:commit
+set /p msg="Enter commit message: "
 if "%msg%"=="" set msg="Update GCP Lab Report"
 
 echo [1/4] Adding files...
 git add .
 
-echo [2/4] Committing changes...
-git commit -m "%msg%"
+echo [2/4] Committing...
+:: Dùng tham số -a để tránh lỗi partial commit trong khi merge
+git commit -a -m %msg%
 
-echo [3/4] Pulling latest changes from remote...
-git pull origin main
+echo [3/4] Syncing with Remote...
+:: Rebase giúp lịch sử commit sạch hơn, đúng chuẩn Senior
+git pull --rebase origin main
 
 echo [4/4] Pushing to GitHub...
 git push origin main
 
 echo ------------------------------------------
-echo DONE! Project updated successfully.
+echo SUCCESS: Project is now in sync with GitHub.
 pause
